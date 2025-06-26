743fc661a02b478ea84fbcd1e40b61dce8edf75a
mjljy <mjljy0379@163.com>
2025-06-26 17:54:13 +0800

    'auto_add'

--- charpter_4_8_4_pointer.cpp
+++ charpter_4_8_4_pointer.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace  std;
 char* getname(void);   // function prototype
+int demo1();
+int demo2();
+int demo3();
+
 struct inflatable {
 
 	char name[20];
@@ -10,25 +14,106 @@ struct inflatable {
 
 int main() 
 {
-    //int price1;
-    //int price2;
-    //inflatable* ps = new inflatable; // allot memory for structure
-    //cout << "Enter name of inflatable item: ";
-    //cin.get(ps->name, 20);            // method 1 for member access
-    //cout << "Enter volume in cubic feet: ";
-    //cin >> (*ps).volume;              // method 2 for member access
-    //cout << "Enter price: $";
-    //cin >> ps->price;
-    //cout << "Name: " << (*ps).name << endl;              // method 2
-    //cout << "Volume: " << ps->volume << " cubic feet\n"; // method 1
-    //cout << "Price ps->price    : $" << ps->price << endl;             // method 1
-    //cout << "Price (*ps).name   : $" << (*ps).price << endl;
-    //price1 = ps->price;
-    //price2 = (*ps).price;
-    //cout << "Price1: $" << price1 << endl;
-    //cout << "Price2: $" << price2 << endl;
-    //delete ps;    // free memory used by structure
-    //cout << "------------------------------------------------"  << endl;
+
+    //demo1();
+    //demo2();
+    demo3();
+
+    return 0;
+}
+
+int demo3()
+{
+
+    struct antractica_years_end {
+        int year;
+    };
+
+    antractica_years_end s01, s02, s03;
+    s01 = { 1998 };
+    s02 = { 1999 };
+    s03 = { 2000 };
+    //创建结构数组 元素需要先初始化
+    antractica_years_end  arp_arry[3] = { s01, s02, s03 };
+    s01.year = 1998;
+    antractica_years_end  trio[3];
+    trio[0].year = 2003;
+    //*(trio+1).year = 2004; 没有结构名字的结构不能使用句点运算符 只能用箭头运算符
+    (trio + 1)->year = 2004;
+
+    cout << "arp_arry[0].year = " <<  arp_arry[0].year << endl;
+    // 创建结构指针数组
+    antractica_years_end* arp[3] = { &s01,&s02,&s03 };
+    cout << "arp[0].year = " << arp[0]->year << endl;
+    cout << "arp[0].year = " << (*arp[0]).year << endl;
+
+    //antractica_years_end * * ppa = arp;
+    antractica_years_end * * ppa1[1] = { arp };
+    auto ppa = arp;
+    //auto ppa1[1] = { arp };
+
+    //cout << "ppa arp[0].year = " << (*(*(ppa)))[0].year << endl;
+    cout << "ppa arp[0].year = " <<  (*(*ppa)).year << endl;
+    cout << "ppa1 arp[0].year = " << (*(*ppa)).year<< endl;
+    cout << "-------------------------------------" << endl;
+    cout << "ppa -> arp[0].year = " << (*ppa)->year << endl;
+
+
+    return 0;
+
+
+}
+
+int demo2()
+{
+    char* pointer_[5];
+    char* pointer_1;;
+    char name[20] = ">>>>>>>>>>>>>>>>";
+    pointer_1 = name;
+    cout << "pointer_1 = " << pointer_1 << endl;
+    cout << "name = " << name << endl;
+    cout << "pointer_1 = " << *pointer_1 << endl;
+    pointer_[0] = pointer_1;
+    cout << "pointer_[0] = " << pointer_[0] << endl;
+
+    cout << "-------------------------------------" << endl;
+
+    inflatable* pointer_2;
+
+    inflatable inf1 = {
+        "every",
+        100.1,
+        100.2 };
+    pointer_2 = &inf1;
+    cout << "pointer_2 = " << (*pointer_2).name << endl;
+    //pointer_[1] = &inf1;
+
+    return 0;
+
+}
+
+int demo1()
+{
+
+    int price1;
+    int price2;
+    inflatable* ps = new inflatable; // allot memory for structure
+    cout << "Enter name of inflatable item: ";
+    cin.get(ps->name, 20);            // method 1 for member access
+    cout << "Enter volume in cubic feet: ";
+    cin >> (*ps).volume;              // method 2 for member access
+    cout << "Enter price: $";
+    cin >> ps->price;
+    cout << "Name: " << (*ps).name << endl;              // method 2
+    cout << "Volume: " << ps->volume << " cubic feet\n"; // method 1
+    cout << "Price ps->price    : $" << ps->price << endl;             // method 1
+    cout << "Price (*ps).name   : $" << (*ps).price << endl;
+    price1 = ps->price;
+    price2 = (*ps).price;
+    cout << "Price1: $" << price1 << endl;
+    cout << "Price2: $" << price2 << endl;
+    delete ps;    // free memory used by structure
+    cout << "------------------------------------------------"  << endl;
 
     char* name;        // create pointer but no storage
     name = getname();   // assign address of string to name
@@ -41,7 +126,9 @@ int main()
     delete[] name;     // memory freed again
 
     return 0;
+
 }
+
 
 char * getname()        // return pointer to new string
 {
