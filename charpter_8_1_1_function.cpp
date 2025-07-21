2747c04b568c8230f327c7b51e8883119f77cb58
mjljy <mjljy0379@163.com>
2025-07-21 18:02:57 +0800

    'auto_add'

--- charpter_8_1_1_function.cpp
+++ charpter_8_1_1_function.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 inline double square(double x) { return  x * x; };
 
-int main() {
+int main811() {
 
 	double x = 2.2;
 	cout << "square = " << square(x) << endl;
---
+++ charpter_8_2_1_function.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+
+	int rats=101;
+	int& dodents = rats;
+
+	int rars1 = rats;
+
+	cout << "rats     =" << rats << endl;
+	cout << "dodents  =" << dodents << endl;
+	cout << "rars1    =" << rars1 << endl;
+
+	cout << "rats     =" << &rats << endl;
+	cout << "dodents  =" << &dodents << endl;
+	cout << "rars1    =" << &rars1 << endl;
+	
+	cout << "-----------------------------------------------" << endl;
+
+	rars1 = 222;
+
+	cout << "rats     =" << rats << endl;
+	cout << "dodents  =" << dodents << endl;
+	cout << "rars1    =" << rars1 << endl;
+
+	cout << "rats     =" << &rats << endl;
+	cout << "dodents  =" << &dodents << endl;
+	cout << "rars1    =" << &rars1 << endl;
+	cout << "-----------------------------------------------" << endl;
+
+	rats++;
+
+	cout << "rats     =" << rats << endl;
+	cout << "dodents  =" << dodents << endl;
+	cout << "rars1    =" << rars1 << endl;
+
+	cout << "rats     =" << &rats << endl;
+	cout << "dodents  =" << &dodents << endl;
+	cout << "rars1    =" << &rars1 << endl;
+
+	cout << "-----------------------------------------------" << endl;
+
+
+	int business = 50;
+	dodents = business;
+
+	cout << "business     =" << business << endl;
+	cout << "business     =" << &business << endl;
+	cout << "---------" << endl;
+	cout << "rats     =" << rats << endl;
+	cout << "dodents  =" << dodents << endl;
+	cout << "rars1    =" << rars1 << endl;
+
+	cout << "rats     =" << &rats << endl;
+	cout << "dodents  =" << &dodents << endl;
+	cout << "rars1    =" << &rars1 << endl;
+
+	cout << "-----------------------------------------------" << endl;
+	dodents = business;
+
+	cout << "business     =" << business << endl;
+	cout << "business     =" << &business << endl;
+	cout << "---------" << endl;
+	cout << "rats     =" << rats << endl;
+	cout << "dodents  =" << dodents << endl;
+	cout << "rars1    =" << rars1 << endl;
+
+	cout << "rats     =" << &rats << endl;
+	cout << "dodents  =" << &dodents << endl;
+	cout << "rars1    =" << &rars1 << endl;
+
+
+	return 0;
+
+}
