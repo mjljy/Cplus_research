b779a565ffcfe8b8543c3b153c7fa96e46a25cb3
lijinyuan <mjljy0379@163.com>
2025-08-03 02:24:35 +0800

    'auto_add'

--- charpter_3_2_char.cpp
+++ charpter_3_2_char.cpp
@@ -4,10 +4,18 @@ using namespace std;
 
 
 //  char 整型
-int main_3_2()
+int main32()
 {
 	char ch = 'M';
 	int i = ch;
+	
+	/*
+	int i = 10;
+	char i = 'x';
+	严重性	代码	说明	项目	文件	行	禁止显示状态	详细信息
+	错误	C2371	“i”: 重定义；不同的基类型	Cplus_research	E:\Github_person\Cplus_research\charpter_3_2_char.cpp	11		
+
+	*/
 	cout << "The ASCII code for " << ch << " is " << i << endl;
 
 	cout << "ADD one to the character  code  " << endl;
--- charpter_8_5_0_function.cpp
+++ charpter_8_5_0_function.cpp
@@ -1,10 +1,44 @@
+// funtemp.cpp -- using a function template
 #include <iostream>
-using namespace std;
+// function template prototype
+template <typename T>  // or class T
+void Swap(T& a, T& b);
+//void Swap(T a, T b);
 
+int main850()
+{
+    using namespace std;
+    int i = 10;
+    int j = 20;
+    cout << "i, j = " << i << ", " << j << ".\n";
+    cout << "Using compiler-generated int swapper:\n";
+    Swap(i, j);  // generates void Swap(int &, int &)
+    cout << "Now i, j = " << i << ", " << j << ".\n";
 
-int main() {
+    double x = 24.5;
+    double y = 81.7;
+    cout << "x, y = " << x << ", " << y << ".\n";
+    cout << "Using compiler-generated double swapper:\n";
+    Swap(x, y);  // generates void Swap(double &, double &)
+    cout << "Now x, y = " << x << ", " << y << ".\n";
+    // cin.get();
+    char x1 = 'x';
+    char y1 = 'y';
+    cout << "x1, y1 = " << x1 << ", " << y1 << ".\n";
+    cout << "Using compiler-generated double swapper:\n";
+    Swap(x1, y1);  // generates void Swap(double &, double &)
+    cout << "Now x1, y1 = " << x1 << ", " << y1 << ".\n";
 
+    return 0;
+}
 
-
-	return 0;
+// function template definition
+template <typename T>  // or class T
+//void Swap(T  a, T  b)
+void Swap(T& a, T& b)
+{
+    T temp;   // temp a variable of type T
+    temp = a;
+    a = b;
+    b = temp;
 }
